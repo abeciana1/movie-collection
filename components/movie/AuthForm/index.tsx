6f3420fb67e419c8dbaee81ad17b9c9f30aa1d5f
abeciana1 <alex.beciana@gmail.com>
2026-09-07 21:39:57 -0600

    auth-form: Added logger import and added handling of google

--- components/movie/AuthForm/index.tsx
+++ components/movie/AuthForm/index.tsx
@@ -7,50 +7,51 @@ import {
   CardContent
 } from "@/components/ui/card";
 import Google from "@/social-logos/Google";
-// import { authClient } from "@/src/utils/auth/auth-client";
+import { authClient } from "@/lib/auth/auth-client"
+import logger from "@/lib/helpers/logger";
 
-// const authOptions = {
-//   callbackURL: "/welcome",
-//   errorCallbackURL: "/",
-//   newUserCallbackURL: "/welcome",
-// };
+const authOptions = {
+  callbackURL: "/welcome",
+  errorCallbackURL: "/",
+  newUserCallbackURL: "/welcome",
+};
 
 const AuthForm = () => {
-  // const handleGoogle = async () => {
-  //   try {
-  //     await authClient.signIn.social(
-  //       {
-  //         provider: "google",
-  //         ...authOptions,
-  //       },
-  //       {
-  //         onRequest: (ctx) => {
-  //           logger(
-  //             "User is about to sign in",
-  //             `ctx:\n${JSON.stringify(ctx, null, 2)}`
-  //           );
-  //         },
-  //         onSuccess: (ctx) => {
-  //           logger(
-  //             "User has signed in",
-  //             `ctx:\n${JSON.stringify(ctx, null, 2)}`
-  //           );
-  //         },
-  //         onError: (ctx) => {
-  //           logger(
-  //             "User failed to sign in",
-  //             `ctx:\n${JSON.stringify(ctx, null, 2)}`
-  //           );
-  //         },
-  //       }
-  //     );
-  //   } catch (error) {
-  //     logger(
-  //       "User failed to sign in with Google",
-  //       `error: \n${JSON.stringify(error, null, 2)}`
-  //     );
-  //   }
-  // };
+  const handleGoogle = async () => {
+    try {
+      await authClient.signIn.social(
+        {
+          provider: "google",
+          ...authOptions,
+        },
+        {
+          onRequest: (ctx) => {
+            logger(
+              "User is about to sign in",
+              `ctx:\n${JSON.stringify(ctx, null, 2)}`
+            );
+          },
+          onSuccess: (ctx) => {
+            logger(
+              "User has signed in",
+              `ctx:\n${JSON.stringify(ctx, null, 2)}`
+            );
+          },
+          onError: (ctx) => {
+            logger(
+              "User failed to sign in",
+              `ctx:\n${JSON.stringify(ctx, null, 2)}`
+            );
+          },
+        }
+      );
+    } catch (error) {
+      logger(
+        "User failed to sign in with Google",
+        `error: \n${JSON.stringify(error, null, 2)}`
+      );
+    }
+  };
 
   return (
     <Card enableGradient className="mx-auto w-70 md:w-100">
