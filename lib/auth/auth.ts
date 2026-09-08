ea68657c12410e53fc1189bee94a3e0257baef9b
abeciana1 <alex.beciana@gmail.com>
2026-09-07 22:12:36 -0600

    auth-form: Changed app name and added advanced config options

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,10 +1,25 @@
 import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
 import { prisma } from "@/lib/prisma";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 
+const isHttps = process.env.BETTER_AUTH_BASE_URL?.startsWith("https://");
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: "sqlite" }),
-  appName: "Movie Collection",
+  appName: "Rewatchr",
   baseURL: process.env.BETTER_AUTH_URL,
-  secret: process.env.BETTER_AUTH_SECRET
+  secret: process.env.BETTER_AUTH_SECRET,
+  logger: {
+    level: "debug",
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      prompt: "select_account consent",
+    },
+  },
+  plugins: [nextCookies()],
+  advanced: { useSecureCookies: !!isHttps, cookiePrefix: "rewatchr" },
 });
